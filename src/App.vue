46694092d9350b4f4a8a8c33380aca484ca25fc2
Benoît Burgener <benoit.burgener@gmail.com>
2020-01-14 11:52:36 +0100

    Add form to add items

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,27 @@
   <div id="app" class="p-4 mx-auto max-w-xl">
     <h1 class="text-4xl mb-6">Shopping list</h1>
 
+    <form
+      action=""
+      class="flex items-center p-4 mb-10 bg-white rounded shadow"
+      @submit.prevent="addItem"
+    >
+      <label for="item" class="sr-only">Item name</label>
+      <input
+        id="item"
+        v-model="form.name"
+        type="text"
+        class="flex-grow border border-gray-500 rounded py-2 px-4"
+        placeholder="Item name"
+      />
+      <button
+        type="submit"
+        class="ml-3 py-2 px-4 text-white font-semibold bg-blue-600 rounded"
+      >
+        Add
+      </button>
+    </form>
+
     <ul class="my-6 border-b border-gray-400">
       <li
         v-for="(item, i) in items"
@@ -20,6 +41,9 @@ export default {
 
   data() {
     return {
+      form: {
+        name: '',
+      },
       items: [
         {
           name: 'Bananas',
@@ -32,6 +56,19 @@ export default {
         },
       ],
     };
+  },
+
+  methods: {
+    addItem() {
+      const name = this.form.name.trim();
+
+      if (!name) return;
+
+      this.items.push({
+        name,
+      });
+      this.form.name = '';
+    },
   },
 };
 </script>
