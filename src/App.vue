6be8c3c3ea094c011996571fac3201f823e66483
Benoît Burgener <benoit.burgener@gmail.com>
2020-01-14 10:35:57 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app" class="p-4 mx-auto max-w-xl">
+    <h1 class="text-4xl">Shopping list</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style></style>
