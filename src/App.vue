109bdb5e7378f24a5954483cc3e2a24418e025ed
Benoît Burgener <benoit.burgener@gmail.com>
2020-01-14 11:21:34 +0100

    List items from data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
   <div id="app" class="p-4 mx-auto max-w-xl">
-    <h1 class="text-4xl">Shopping list</h1>
+    <h1 class="text-4xl mb-6">Shopping list</h1>
+
+    <ul class="my-6 border-b border-gray-400">
+      <li
+        v-for="(item, i) in items"
+        :key="i"
+        class="py-2 border-t border-gray-400"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+
+  data() {
+    return {
+      items: [
+        {
+          name: 'Bananas',
+        },
+        {
+          name: 'Tofu',
+        },
+        {
+          name: 'Milk',
+        },
+      ],
+    };
+  },
 };
 </script>
 
